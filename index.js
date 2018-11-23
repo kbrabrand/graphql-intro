bf23e41070cbc2128ea2ec5c01b23cf113ddd7e7
Kristoffer Brabrand <kristoffer@brabrand.no>
2018-11-23 02:39:06 +0100

    Add engine key to server init

--- index.js
+++ index.js
@@ -82,7 +82,13 @@ const resolvers = {
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  engine: {
+    apiKey: "service:kbrabrand-6658:sIMJSjs3YI5RD6eiMydhRg"
+  }
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
